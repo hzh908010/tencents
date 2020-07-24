f5eece6a957c6f29ab6871ad04b8cf89dca8ab6b
hzh <864416859@qq.com>
2020-07-24 14:00:20 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// var boxs = document.querySelector('nav_box');
+// boxs.onclick = function() {
+
+// }
+// var section = boxs.querySelector('section');
+// section.onclick = function(){
+
+// }
+// var hs = nav.getElementsByTagName('h5');
+// var as = hs.getElementsByTagName('a');
+// var hs = nav.querySelectorAll('h5');
+// var as = nav.querySelectorAll('a');
+//   as.onclick = function(){
+//       console.log(111);
+//   }
+// var sections = document.querySelectorAll('.section');
+// for (var i = 0; i< as.length;i++) {
+//     as[i].setAttribute('index',i);
+//     as[i].onmousemove = function(){
+//       sections[index].style.display = 'block';
+//     }
+// }
